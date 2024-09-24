3d0c98e2e51f965d2b912a48e9133246fc1d4f2b
meshchellomishel <126414636+meshchellomishel@users.noreply.github.com>
2024-09-24 19:27:12 +0300

    Update lag-test.sh

--- lag-test.sh
+++ lag-test.sh
@@ -27,7 +27,7 @@ done
 create_lag () {
 	ip l add lag"$1" type bond mode 2
 	ip l set lag"$1" up
-} 
+}
 
 delete_lag() {
 	ip l del lag"$1"
@@ -44,32 +44,36 @@ release_port () {
 }
 
 if [ "$OPEXIST" != "true" ]; then
-  echo "No option specified"
+  echo "No option specified1"
   exit 0
 fi
 
-if [ "$ENSLAVETEST" = "true" ]; then
-  echo "Creating lag$1"
-  create_lag 1
-fi
-
-PORTS="fe1 fe2 fe3 fe4 fe5 fe6 fe7 fe8 fe9 fe10 fe11 fe12 fe13 fe14 fe15 fe16 fe17 fe18"
+PORTS="fe1 fe2 fe3 fe4 fe5 fe6 fe7 fe8 fe9 fe10 fe11 fe12 fe13 fe14 fe15 fe16 fe17 fe18 fe19 fe20 fe21 fe22 fe23 fe24 ge1 ge3 ge4"
 # fe10 fe11 fe12 fe13 fe14 fe15 fe16 fe17 fe18 fe19 fe20 fe21 fe22 fe23 fe24
+ENSLAVETESTPORTS="ge1 fe1 fe2 fe3 fe4 fe5 fe6 fe7"
 
-c=1
-for i in $PORTS
-do
-	if [ "$LAGTEST" = "true" ]; then
-  	echo "Enslaving $i to lag1"
-		enslave_port "$i" 1
-  elif [ "$ENSLAVETEST" = "true" ]; then
-  	echo "Enslaving $i"
-		enslave_port 1
-	  c=$(($c+1))
-	elif [ "$RELEASE" = "true" ]; then
-  	echo "Releasing $i"
-		delete_lag "$c"
-		release_port "$i"
-	  c=$(($c+1))
-	fi
-done
+if [ "$ENSLAVETEST" != "true" ]; then
+    c=1
+    for i in $PORTS
+    do
+        if [ "$LAGTEST" = "true" ]; then
+            echo "Creating lag$c"
+            create_lag "$c"
+            echo "Enslaving $i to lag$c"
+            enslave_port "$i" "$c"
+        elif [ "$RELEASE" = "true" ]; then
+            echo "Releasing $i"
+            delete_lag "$c"
+            release_port "$i"
+        fi
+        c=$(($c+1))
+    done
+else
+    echo "Creating lag1"
+    create_lag 1
+    for i in $ENSLAVETESTPORTS
+    do
+        echo "Enslaving $i"
+        enslave_port "$i" 1
+    done
+fi
